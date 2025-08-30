17e9c16e5cbf42a920314e994bbdb718c184edc6
Cuchillon <ferickadmin@gmail.com>
2025-08-30 14:14:33 +0300

    hw2: add to-do and change buttons styles

--- src/app/components/to-do-list/to-do-list.ts
+++ src/app/components/to-do-list/to-do-list.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, computed, model, ModelSignal, Signal } from '@angular/core';
+import { ToDo } from '../../model/to-do';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-list',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './to-do-list.html',
   styleUrl: './to-do-list.scss'
 })
 export class ToDoList {
-  protected readonly toDoList = [
-    'Посадить печень',
-    'Вырастить пузо',
-    'Построить тещу'
+  protected newTask: ModelSignal<string> = model<string>('');
+  protected isNewTaskEmpty: Signal<boolean> = computed(() => this.newTask().length === 0);
+
+  protected readonly toDoList: ToDo[] = [
+    {
+      id: 1,
+      text: 'Посадить печень'
+    },
+    {
+      id: 2,
+      text: 'Вырастить пузо'
+    },
+    {
+      id: 3,
+      text: 'Построить тещу'
+    }
   ];
 }
