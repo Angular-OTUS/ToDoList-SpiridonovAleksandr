84d67a78e9e7399c609a57019a70a3c9fbbffc5c
Cuchillon <ferickadmin@gmail.com>
2025-11-14 22:24:03 +0300

    hw9: add store into board

--- src/app/components/modules/board/board.ts
+++ src/app/components/modules/board/board.ts
@@ -1,27 +1,15 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  DestroyRef,
-  inject,
-  Signal,
-  signal,
-  WritableSignal
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { EmptyList } from '../../shared/empty-list/empty-list';
 import { LoadingSpinner } from '../../shared/loading-spinner/loading-spinner';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ToDoListItem } from '../to-do-list-item/to-do-list-item';
 import { TODO_TOAST_MESSAGES } from '../../../tokens/to-do-toast.token';
-import { ToDoListApiService } from '../../../services/to-do-list.api.service';
 import { ToastService } from '../../../services/toast.service';
-import { ToDoEventService } from '../../../services/to-do-event.service';
 import { ToastType } from '../../../model/toast-dto';
-import { finalize, Observable, startWith, Subject, switchMap } from 'rxjs';
-import { ToDo, ToDoDto, ToDos, ToDoStatus } from '../../../model/to-do';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { ToDo, ToDoDto, ToDoStatus } from '../../../model/to-do';
 import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
+import { ToDoStore } from '../../../state/to-do.store';
 
 @Component({
   selector: 'app-board',
@@ -47,70 +35,24 @@ import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Board {
-  private readonly destroyRef: DestroyRef = inject(DestroyRef);
-  private readonly toDoListService: ToDoListApiService = inject(ToDoListApiService);
   private readonly router: Router = inject(Router);
-  private readonly route: ActivatedRoute = inject(ActivatedRoute);
   private readonly toastService: ToastService = inject(ToastService);
-  private readonly toDoEventService: ToDoEventService = inject(ToDoEventService);
   private readonly toastMessages: Record<ToastType, string> = inject(TODO_TOAST_MESSAGES);
-
-  protected isLoading: WritableSignal<boolean> = signal<boolean>(false);
-
-  private refreshTrigger$: Subject<void> = new Subject<void>();
-  private toDos$: Observable<ToDos> = this.refreshTrigger$.pipe(
-    startWith(undefined),
-    switchMap(() => {
-      this.isLoading.set(true);
-      return this.toDoListService.getAll().pipe(
-        finalize(() => this.isLoading.set(false)),
-      );
-    }),
-  );
-  protected toDos: Signal<ToDos> = toSignal(this.toDos$, {
-    initialValue: { items: [] }
-  });
-
-  protected created: Signal<ToDo[]> = computed(() =>
-    this.toDos().items.filter(item => item.status === 'CREATED')
-  );
-
-  protected inProgress: Signal<ToDo[]> = computed(() =>
-    this.toDos().items.filter(item => item.status === 'IN_PROGRESS')
-  );
-
-  protected completed: Signal<ToDo[]> = computed(() =>
-    this.toDos().items.filter(item => item.status === 'COMPLETED')
-  );
+  protected readonly store = inject(ToDoStore);
 
   protected addTask(task: ToDoDto) {
-    this.isLoading.set(true);
-    this.toDoListService.add(task).subscribe({
-      next: () => {
-        this.refreshTrigger$.next();
-      },
-    });
+    this.store.add(task);
     this.toastService.showToast(this.toastMessages.success, 'success');
   }
 
   protected deleteTask(id: number) {
-    this.isLoading.set(true);
-    this.toDoListService.removeById(id).subscribe({
-      next: () => {
-        this.refreshTrigger$.next();
-      },
-    });
+    this.store.removeById(id);
     this.toastService.showToast(this.toastMessages.warning, 'warning');
     this.router.navigate(['board']);
   }
 
   protected saveTask(toDo: ToDo) {
-    this.isLoading.set(true);
-    this.toDoListService.update(toDo).subscribe({
-      next: () => {
-        this.refreshTrigger$.next();
-      },
-    });
+    this.store.update(toDo);
     this.hideAllTooltips();
     this.toastService.showToast(this.toastMessages.info, 'info');
   }
